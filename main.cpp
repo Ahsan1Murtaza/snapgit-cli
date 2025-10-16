3ab504df3c6ebf638a70a408bf4df6b93e929bd4
Tushar Lalwani <lalwanit34@gmail.com>
2025-10-17 00:12:41 +0500

    Refactor main.cpp for Git repository initialization

--- main.cpp
+++ main.cpp
@@ -6,16 +6,14 @@
 
 using namespace std;
 
-// Check if directory exists
 bool dirExists(const string& path) {
     struct stat info;
     return (stat(path.c_str(), &info) == 0 && (info.st_mode & S_IFDIR));
 }
 
-// Create directory with error checking
 bool createDir(const string& path) {
     if (dirExists(path)) {
-        return true;  
+        return true;
     }
     return _mkdir(path.c_str()) == 0;
 }
@@ -28,35 +26,22 @@ void printHelp() {
 }
 
 void handleInit() {
-    // Check if already initialized
     if (dirExists(".git")) {
         cout << "Reinitialized existing Git repository in .git/\n";
         return;
     }
-    
-    // Create main .git directory
+
     if (!createDir(".git")) {
         cerr << "Error: Failed to create .git directory\n";
         return;
     }
-    
-    // Create objects directory structure
+
     createDir(".git\\objects");
-    createDir(".git\\objects\\pack");
-    createDir(".git\\objects\\info");
-    
-    // Create refs directory structure
     createDir(".git\\refs");
     createDir(".git\\refs\\heads");
-    createDir(".git\\refs\\tags");
-    
-    // Create info directory
     createDir(".git\\info");
-    
-    // Create hooks directory
     createDir(".git\\hooks");
-    
-    // Create HEAD file
+
     ofstream headFile(".git\\HEAD");
     if (!headFile) {
         cerr << "Error: Failed to create HEAD file\n";
@@ -64,7 +49,7 @@ void handleInit() {
     }
     headFile << "ref: refs/heads/main\n";
     headFile.close();
-    
+
     ofstream configFile(".git\\config");
     if (configFile) {
         configFile << "[core]\n";
@@ -76,45 +61,22 @@ void handleInit() {
         configFile << "\tignorecase = true\n";
         configFile.close();
     }
-    
+
     ofstream descFile(".git\\description");
     if (descFile) {
         descFile << "Unnamed repository; edit this file 'description' to name the repository.\n";
         descFile.close();
     }
-    
+
     ofstream excludeFile(".git\\info\\exclude");
     if (excludeFile) {
         excludeFile << "# git ls-files --others --exclude-from=.git/info/exclude\n";
         excludeFile << "# Lines that start with '#' are comments.\n";
-        excludeFile << "# For a project mostly in C, the following would be a good set of\n";
-        excludeFile << "# exclude patterns (uncomment them if you want to use them):\n";
         excludeFile << "# *.[oa]\n";
         excludeFile << "# *~\n";
         excludeFile.close();
     }
-    
-    ofstream preCommitHook(".git\\hooks\\pre-commit.sample");
-    if (preCommitHook) {
-        preCommitHook << "#!/bin/sh\n";
-        preCommitHook << "#\n";
-        preCommitHook << "# An example hook script to verify what is about to be committed.\n";
-        preCommitHook << "# Called by \"git commit\" with no arguments. The hook should\n";
-        preCommitHook << "# exit with non-zero status after issuing an appropriate message if\n";
-        preCommitHook << "# it wants to stop the commit.\n";
-        preCommitHook.close();
-    }
-    
-    ofstream commitMsgHook(".git\\hooks\\commit-msg.sample");
-    if (commitMsgHook) {
-        commitMsgHook << "#!/bin/sh\n";
-        commitMsgHook << "#\n";
-        commitMsgHook << "# An example hook script to check the commit log message.\n";
-        commitMsgHook << "# Called by \"git commit\" with one argument, the name of the file\n";
-        commitMsgHook << "# that has the commit message.\n";
-        commitMsgHook.close();
-    }
-    
+
     cout << "Initialized empty Git repository in .git/\n";
 }
 
@@ -123,9 +85,9 @@ int main(int argc, char* argv[]) {
         printHelp();
         return 0;
     }
-    
+
     string command = argv[1];
-    
+
     if (command == "init") {
         handleInit();
     } else if (command == "help" || command == "--help" || command == "-h") {
@@ -135,6 +97,6 @@ int main(int argc, char* argv[]) {
         printHelp();
         return 1;
     }
-    
+
     return 0;
 }
