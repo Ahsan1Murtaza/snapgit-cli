83fc1594ece1f12a377046537140e7f04f1a4c2e
Ahsan Murtaza <ahsanmurtaza2005@gmail.com>
2025-10-22 17:28:26 +0500

    Add CommitHandler Integration with commit Class left. Currently using Tree class for Development Purpose

---
+++ CommandHandlers/CommitHandler/CommitHandler.cpp
@@ -0,0 +1,30 @@
+
+
+// It will manage with commit class
+// But for now it will use Tree class to check functionality
+
+#include "CommitHandler.h"
+#include "../../Objects/Tree/Tree.h"
+
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include "../../Helper/RepoCheck/RepoCheck.h"
+
+using namespace std;
+
+CommitHandler::CommitHandler() {};
+
+void CommitHandler::handleCommit(const string &message)
+{
+    if (!isRepoInitialized()) {
+        cerr << "Error: Repository not initialized. Please run 'init' command first.\n";
+        return;
+    }
+
+    Tree tree;
+    tree.buildFromIndex(".mygit/index");
+    tree.showTreeElements();
+
+    cout << "Committed with message: " << message << "\n";
+}
