0e7dc55b1cd1fa69545d554d78940d672a1bf5b0
Ahsan Murtaza <ahsanmurtaza2005@gmail.com>
2025-10-25 07:01:06 +0500

    Changes- Change commiting to refs/heads/main to refs/head/<bracnh> Reading from Head file

--- Helper/UpdateHead/UpdateHead.cpp
+++ Helper/UpdateHead/UpdateHead.cpp
@@ -1,9 +1,15 @@
 #include "UpdateHead.h"
-
+#include "../GetHeadRef/GetHeadRef.h"
 #include <fstream>
+#include <string>
+#include <iostream>
+using namespace std;
 
-void updateHead(const std::string& newHash) {
-    std::ofstream out(".mygit/refs/heads/main", std::ios::trunc);
+void updateHead(const string& newHash) {
+    string refFile = getHeadRef();
+    string path = ".mygit/" + refFile;
+    cout << "Commit is being done to Branch ref " << path << endl;
+    std::ofstream out(path, std::ios::trunc);
 
     out << newHash;
     out.close();
