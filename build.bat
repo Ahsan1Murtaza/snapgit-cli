45fbb1308f6818c5f872c12cac4cc2342bca5949
Ahsan Murtaza <ahsanmurtaza2005@gmail.com>
2025-10-26 10:21:10 +0500

    Add build.bat

---
+++ build.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Building mygit CLI...
+
+g++ .\main.cpp .\CommandHandlers\AddHandler\AddHandler.cpp .\CommandHandlers\InitHandler\InitHandler.cpp .\CommandHandlers\CommitHandler\CommitHandler.cpp .\Helper\Hash\Hash.cpp .\Objects\Blob\Blob.cpp .\Objects\Tree\Tree.cpp .\Objects\Commit\Commit.cpp .\Helper\RepoCheck\RepoCheck.cpp .\Helper\SortIndex\SortIndex.cpp .\Helper\GetCurrentCommitHash\GetCurrentCommitHash.cpp .\Helper\UpdateHead\UpdateHead.cpp .\Helper\GetHeadRef\GetHeadRef.cpp .\CommandHandlers\ConfigHandler\ConfigHandler.cpp .\Helper\GetUserInfo\GetUserInfo.cpp -o mygit.exe -lssl -lcrypto
+
+echo.
+echo Build complete! Run mygit.exe to test.
+pause
