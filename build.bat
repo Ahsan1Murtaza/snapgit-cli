6fca0d5242227b5efe2dae48fe59ff958f7be28c
Ahsan Murtaza <ahsanmurtaza2005@gmail.com>
2025-11-23 07:38:06 +0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,25 +1,6 @@
 @echo off
 echo Building mygit CLI...
-g++ .\main.cpp ^
-.\CommandHandlers\AddHandler\AddHandler.cpp ^
-.\CommandHandlers\InitHandler\InitHandler.cpp ^
-.\CommandHandlers\CommitHandler\CommitHandler.cpp ^
-.\CommandHandlers\ConfigHandler\ConfigHandler.cpp ^
-.\CommandHandlers\BranchHandler\BranchHandler.cpp ^
-.\CommandHandlers\LogHandler\LogHandler.cpp ^
-.\CommandHandlers\CheckoutHandler\CheckoutHandler.cpp ^
-.\Helper\GetAllCommits\GetAllCommits.cpp ^
-.\Helper\Hash\Hash.cpp ^
-.\Helper\RepoCheck\RepoCheck.cpp ^
-.\Helper\SortIndex\SortIndex.cpp ^
-.\Helper\GetCurrentCommitHash\GetCurrentCommitHash.cpp ^
-.\Helper\UpdateHead\UpdateHead.cpp ^
-.\Helper\GetHeadRef\GetHeadRef.cpp ^
-.\Helper\GetUserInfo\GetUserInfo.cpp ^
-.\Helper\GetAllBranches\GetAllBranches.cpp ^
-.\Helper\ReadCommit\ReadCommit.cpp ^
-.\Helper\RestoreTree\RestoreTree.cpp ^
-.\Objects\Blob\Blob.cpp .\Objects\Tree\Tree.cpp .\Objects\Commit\Commit.cpp -o mygit.exe -static -lssl -lcrypto -lcrypt32 -lws2_32
+g++ .\main.cpp .\CommandHandlers\AddHandler\AddHandler.cpp .\CommandHandlers\InitHandler\InitHandler.cpp .\CommandHandlers\CommitHandler\CommitHandler.cpp .\CommandHandlers\ConfigHandler\ConfigHandler.cpp .\CommandHandlers\BranchHandler\BranchHandler.cpp .\CommandHandlers\CheckoutHandler\CheckoutHandler.cpp .\CommandHandlers\LogHandler\LogHandler.cpp .\CommandHandlers\ResetHandler\ResetHandler.cpp .\CommandHandlers\StatusHandler\StatusHandler.cpp .\CommandHandlers\RestoreHandler\RestoreHandler.cpp .\CommandHandlers\RemoveHandler\RemoveHandler.cpp .\CommandHandlers\MergeHandler\MergeHandler.cpp .\Helper\Hash\Hash.cpp .\Helper\RepoCheck\RepoCheck.cpp .\Helper\SortIndex\SortIndex.cpp .\Helper\GetCurrentCommitHash\GetCurrentCommitHash.cpp .\Helper\UpdateHead\UpdateHead.cpp .\Helper\GetHeadRef\GetHeadRef.cpp .\Helper\GetUserInfo\GetUserInfo.cpp .\Helper\GetAllBranches\GetAllBranches.cpp .\Helper\ReadCommit\ReadCommit.cpp .\Helper\RestoreTree\RestoreTree.cpp .\Helper\GetAllCommits\GetAllCommits.cpp .\Helper\ReadIndex\ReadIndex.cpp  .\Objects\Blob\Blob.cpp .\Objects\Tree\Tree.cpp .\Objects\Commit\Commit.cpp -o mygit.exe -lssl -lcrypto
 echo.
 echo Build complete! Run mygit.exe to test.
 pause
